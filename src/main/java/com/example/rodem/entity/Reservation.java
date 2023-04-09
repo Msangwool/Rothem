fba24538a9b7e368167de160bcbb7b91280af276
OpenTheDoor <tkddnwkdb@gmail.com>
2023-04-10 03:32:45 +0900

    feat: Add Room and Reservation Entity

--- src/main/java/com/example/rodem/entity/Reservation.java
+++ src/main/java/com/example/rodem/entity/Reservation.java
@@ -1,2 +1,22 @@
-package com.example.rodem.entity;public class Reservation {
+package com.example.rodem.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Reservation {
+    private Long id;
+    private Room room;
+    private Long userId;
+    private boolean isAccepted; // 예약 처리 여부 (관리자가 승인 해야 true)
+
+    @Builder
+    public Reservation(Long id, Room room, Long userId, boolean isAccepted) {
+        this.id = id;
+        this.room = room;
+        this.userId = userId;
+        this.isAccepted = isAccepted;
+    }
 }
--- src/main/java/com/example/rodem/entity/Room.java
+++ src/main/java/com/example/rodem/entity/Room.java
@@ -1,2 +1,20 @@
-package com.example.rodem.entity;public class Room {
+package com.example.rodem.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Room {
+    private Long id;
+    private String name;
+
+    @Builder
+    public Room(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
