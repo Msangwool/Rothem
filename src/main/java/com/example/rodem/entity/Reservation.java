9be293971c51436e972911b123c7976b7eb292c3
OpenTheDoor <tkddnwkdb@gmail.com>
2023-04-13 18:15:36 +0900

    refactor: Add Field in Entity

--- src/main/java/com/example/rodem/entity/Reservation.java
+++ src/main/java/com/example/rodem/entity/Reservation.java
@@ -2,21 +2,33 @@ package com.example.rodem.entity;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
+@NoArgsConstructor
 public class Reservation {
-    private Long id;
-    private Room room;
-    private Long userId;
-    private boolean isAccepted; // 예약 처리 여부 (관리자가 승인 해야 true)
+    private Long id;                    // 예약 번호
+    private Long roomId;                // 예약한 방 정보
+    private Long userId;                // 예약한 사용자 ID
+    private Long personNumber;          // 방을 이용하는 사람 수
+    private String reservedTime;        // 요청된 시간
+    private String approvedTime;        // 승인된 시간
+    private String expiredTime;         // 만료되는 시간
+    private Boolean isAccepted;         // 예약 처리 여부 (관리자가 승인 해야 true)
+    private Long availableExtend;       // 연장 가능 횟수
+    private String participateProgram;  // 참여중인 프로젝트
 
     @Builder
-    public Reservation(Long id, Room room, Long userId, boolean isAccepted) {
+    public Reservation(Long id, Long roomId, Long userId, Long personNumber, String reservedTime, String approvedTime, String expiredTime, Boolean isAccepted, Long availableExtend, String participateProgram) {
         this.id = id;
-        this.room = room;
+        this.roomId = roomId;
         this.userId = userId;
+        this.personNumber = personNumber;
+        this.reservedTime = reservedTime;
+        this.approvedTime = approvedTime;
+        this.expiredTime = expiredTime;
         this.isAccepted = isAccepted;
+        this.availableExtend = availableExtend;
+        this.participateProgram = participateProgram;
     }
 }
--- src/main/java/com/example/rodem/entity/Room.java
+++ src/main/java/com/example/rodem/entity/Room.java
@@ -3,18 +3,24 @@ package com.example.rodem.entity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 public class Room {
-    private Long id;
-    private String name;
+    private Long id;                    // 방 ID
+    private String explanation;         // 방 설명 (안내 사항)
+    private Long availablePersonnel;    // 사용 가능한 인원
+    private Boolean isReserved;         // 예약 가능 여부
+    private String environment;         // 방 환경 (대여실에 있는 물품 내역)
+    private String expiredTime;         // 만료되는 시간
 
     @Builder
-    public Room(Long id, String name) {
+    public Room(Long id, String explanation, Long availablePersonnel, Boolean isReserved, String environment, String expiredTime) {
         this.id = id;
-        this.name = name;
+        this.explanation = explanation;
+        this.availablePersonnel = availablePersonnel;
+        this.isReserved = isReserved;
+        this.environment = environment;
+        this.expiredTime = expiredTime;
     }
 }
